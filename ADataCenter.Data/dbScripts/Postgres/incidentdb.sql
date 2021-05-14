3607e6f9b2c7c9d78c05de5aec0eb9b685e36d4e
pruginkad <serovdanil@gmail.com>
2021-05-14 17:47:35 +0300

    db

--- ADataCenter.Data/dbScripts/Postgres/incidentdb.sql
+++ ADataCenter.Data/dbScripts/Postgres/incidentdb.sql
@@ -13,3 +13,45 @@ CREATE DATABASE incidentdb
 
 COMMENT ON DATABASE incidentdb
     IS 'incidentdb';
+-------------------------------------------------------------------------------------------------------------------------
+    -- Table: public.incidents
+
+-- DROP TABLE public.incidents;
+
+CREATE TABLE public.incidents
+(
+    name character varying COLLATE pg_catalog."default",
+    action character varying COLLATE pg_catalog."default",
+    objid character varying COLLATE pg_catalog."default",
+    objtype character varying COLLATE pg_catalog."default",
+    "timestamp" timestamp with time zone,
+    id uuid NOT NULL,
+    user_id character varying COLLATE pg_catalog."default"
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.incidents
+    OWNER to postgres;
+
+
+-----------------------------------------------------------------------------------------------------------------------------------
+
+-- Table: public.incident_handling
+
+-- DROP TABLE public.incident_handling;
+
+CREATE TABLE public.incident_handling
+(
+    line_descr character varying COLLATE pg_catalog."default",
+    line_action character varying COLLATE pg_catalog."default",
+    incident_id uuid,
+    id uuid NOT NULL,
+    line_timestamp timestamp without time zone,
+    image_path character varying COLLATE pg_catalog."default"
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.incident_handling
+    OWNER to postgres;
--- ADataCenter.Data/dbScripts/SqlServer/incidentdb.sql
+++ ADataCenter.Data/dbScripts/SqlServer/incidentdb.sql
@@ -1,15 +1,54 @@
--- Database: incidentdb
+/*CREATE DATABASE incidentdb*/
 
--- DROP DATABASE incidentdb;
 
-CREATE DATABASE incidentdb
-    WITH 
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'Russian_Russia.1251'
-    LC_CTYPE = 'Russian_Russia.1251'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
+declare @dbname varchar(100);
+set @dbname=DB_NAME();
 
-COMMENT ON DATABASE incidentdb
-    IS 'incidentdb';
+if (SELECT user_access_desc FROM sys.databases WHERE name = @dbname) = 'SINGLE_USER'
+BEGIN
+    exec('alter database '+@dbname+' SET MULTI_USER');
+	/*ALTER DATABASE CURRENT  SET MULTI_USER*/
+	/*ALTER DATABASE CURRENT  SET SINGLE_USER*/
+END
+GO
+
+
+
+IF (NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[incidents]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
+	and (NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[incidents]') and xtype = 'SN')))
+BEGIN 
+CREATE TABLE [dbo].incidents( 
+	[name] [nvarchar](255) NULL, 
+	[action] [nvarchar](255) NULL, 
+	[objid] [nvarchar](255) NULL, 
+	[objtype] [nvarchar](255) NULL, 
+	[user_id] [nvarchar](255) NULL, 
+	[timestamp] [datetimeoffset] NULL,
+	[id] [uniqueidentifier] NOT NULL
+ CONSTRAINT [PK_GUID_incidents] PRIMARY KEY CLUSTERED 
+( [id] ASC )
+) ON [PRIMARY] 
+END 
+
+IF (NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[incident_handling]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
+	and (NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[incident_handling]') and xtype = 'SN')))
+BEGIN 
+CREATE TABLE [dbo].incident_handling( 
+	[line_descr] [nvarchar](max) NULL, 
+	[line_action] [nvarchar](max) NULL, 
+	[image_path] [nvarchar](max) NULL, 
+	[incident_id] [uniqueidentifier] NOT NULL, 
+	[line_timestamp] [datetimeoffset] NULL,
+	[id] [uniqueidentifier] NOT NULL
+ CONSTRAINT [PK_GUID_incident_handling] PRIMARY KEY CLUSTERED 
+( [id] ASC )
+) ON [PRIMARY] 
+END 
+
+IF NOT EXISTS (SELECT * FROM dbo.sysindexes WHERE id = OBJECT_ID(N'[dbo].[incident_handling]') AND name = N'IX_incident_id')
+	CREATE NONCLUSTERED INDEX [IX_incident_id] ON [dbo].[incident_handling] 
+	 ([incident_id] ASC)
+
+
+
+
